0b1211b7b44a78e135abf8e86af06ebe1ec4d142
fdnflm <berezin.albert@gmail.com>
2021-09-13 12:40:43 +0300

    - Optimized links filtering - Fixed error on image save

--- downloader.py
+++ downloader.py
@@ -15,13 +15,11 @@ class Downloader:
         for val in range(1, self.pages_val + 1): # Download images on each page for specific resoluiton
             req = requests.get(f"https://wallpaperscraft.com/all/{self.resolution}/page{val}")
             soup = BeautifulSoup(req.text, features="html.parser")
-            payload = []
             result = []
             for wallpaper in soup.find_all("img"): # Parse all urls on the page
-                payload.append(wallpaper.get("src"))
-            for item in payload:
-                if item.startswith("https"): # Append each element that has "https" in the URL
-                    result.append(item)
+                link = wallpaper.get("src")
+                if link.startswith("https://images.wallpaperscraft.com"):
+                    result.append(link)
             for url in result:
                 for tag in self.tags: # And now check all tags for equality
                     if tag in url:
@@ -29,6 +27,6 @@ class Downloader:
                         img = requests.get(replaced, stream=True)
                         if not os.path.exists("saved_images"): # Create a new folder if it doesn't exists already
                             os.makedirs("saved_images")
-                        with open(f"saved_images/{replaced[41:]}", "wb") as f: # Save images to the folder
+                        with open(f"saved_images/{replaced.split('/')[-1:][0]}", "wb") as f: # Save images to the folder
                             for chunk in img:
                                 f.write(chunk)
